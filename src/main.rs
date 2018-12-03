2df433da1d6cf90d67f652ff8e1edbd894857245
Alec Goncharow <Alec.Goncharow@gmail.com>
2018-12-02 22:35:18 -0500

    added multiply to test how args work, working on poll command

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,26 @@
 #[macro_use]
 extern crate serenity;
-
-use serenity::client::Client;
-use serenity::framework::standard::StandardFramework;
-use serenity::prelude::EventHandler;
+use serenity::{framework::StandardFramework, model::gateway::Ready, prelude::*};
 use std::env;
 
 struct Handler;
 
-impl EventHandler for Handler {}
+impl EventHandler for Handler {
+    fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
 fn main() {
     // Login with a bot token from the environment
     let mut client = Client::new(&env::var("DISCORD_BOT_TOKEN").expect("token"), Handler)
         .expect("Error creating client");
     client.with_framework(
         StandardFramework::new()
-            .configure(|c| c.prefix("-")) // set the bot's prefix to "~"
-            .cmd("ping", ping),
+            .configure(|c| c.prefix("-")) // set the bot's prefix to "-"
+            .cmd("ping", ping)
+            .cmd("mort", mort)
+            .cmd("multiply", multiply)
+            .cmd("poll", poll),
     );
 
     // start listening for events by starting a single shard
@@ -30,5 +34,22 @@ command!(ping(_context, message) {
 });
 
 command!(mort(_context, message) {
-    let _ = message.reply("Don't Believe His Lies");
+    let _ = message.channel_id.say("Don't Believe His Lies");
+});
+
+command!(multiply(_ctx, msg, args) {
+    let mut product = 1 as f32;
+    loop {
+        let a = match args.single::<f32>() {
+            Ok(r) => r,
+            Err(_) => break,
+        };
+        product *= a;
+    }
+    let _ = msg.reply(&product.to_string());
+});
+
+command!(poll(_ctx, msg, args) {
+    let re = args.single_quoted::<String>().unwrap();
+    let _ = msg.reply(&re);
 });

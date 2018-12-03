296317f2caf7983a890671c9d56bb45f241ff63d
Alec Goncharow <Alec.Goncharow@gmail.com>
2018-12-02 21:49:37 -0500

    added inital structure

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+#[macro_use]
+extern crate serenity;
+
+use serenity::client::Client;
+use serenity::framework::standard::StandardFramework;
+use serenity::prelude::EventHandler;
+use std::env;
+
+struct Handler;
+
+impl EventHandler for Handler {}
+fn main() {
+    // Login with a bot token from the environment
+    let mut client = Client::new(&env::var("DISCORD_BOT_TOKEN").expect("token"), Handler)
+        .expect("Error creating client");
+    client.with_framework(
+        StandardFramework::new()
+            .configure(|c| c.prefix("-")) // set the bot's prefix to "~"
+            .cmd("ping", ping),
+    );
+
+    // start listening for events by starting a single shard
+    if let Err(why) = client.start() {
+        println!("An error occurred while running the client: {:?}", why);
+    }
+}
+
+command!(ping(_context, message) {
+    let _ = message.reply("Pong!");
+});
+
+command!(mort(_context, message) {
+    let _ = message.reply("Don't Believe His Lies");
+});
